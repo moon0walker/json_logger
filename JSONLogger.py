cd2b26d320a2d86e7362a63334ddb8504a22e8f2
moon0walker <moon0walker@users.noreply.github.com>
2017-01-10 10:21:06 +0300

    add custom filter to Logger, update QueueManager

--- JSONLogger.py
+++ JSONLogger.py
@@ -113,12 +113,24 @@ class JSONFileHandler(WatchedFileHandler):
         except Exception as err:
             print(str(err))
 
+            
+class CustomFilter(logging.Filter):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self._kwargs = deepcopy(kwargs)
 
+    def filter(self, record):
+        list(map(lambda x: setattr(record, x, self._kwargs[x]), self._kwargs))
+        return True
+
+    
 class Logger():
     def __init__(self, *filename):
         # imp.reload(logging)
         self.print = logging.getLogger(reduce(lambda x,y: x+y, filename))
-
+        self.print.propagate = False
+        self.print.addFilter(CustomFilter(**kwargs))
+        
         self.createDir(filename)
 
         logging.SUCCESS = 1
@@ -161,32 +173,49 @@ class LoggerManager:
         startQueueManager(self)
         # self.print = self
 
-    def info(self, logfile, logmsg):
-        self.queue.put_nowait((logfile, sys._getframe().f_code.co_name, logmsg))
+    def info(self, logfile, log_msg, **kwargs):
+        self.queue.put(
+            (logfile, sys._getframe().f_code.co_name, log_msg, kwargs))
 
-    def error(self, logfile, logmsg):
-        self.queue.put_nowait((logfile, sys._getframe().f_code.co_name, logmsg))
+    def error(self, logfile, log_msg, **kwargs):
+        self.queue.put(
+            (logfile, sys._getframe().f_code.co_name, log_msg, kwargs))
 
-    def warning(self, logfile, logmsg):
-        self.queue.put_nowait((logfile, sys._getframe().f_code.co_name, logmsg))
+    def warning(self, logfile, log_msg, **kwargs):
+        self.queue.put(
+            (logfile, sys._getframe().f_code.co_name, log_msg, kwargs))
 
-    def success(self, logfile, logmsg):
-        self.queue.put_nowait((logfile, sys._getframe().f_code.co_name, logmsg))
+    def success(self, logfile, log_msg, **kwargs):
+        self.queue.put(
+            (logfile, sys._getframe().f_code.co_name, log_msg, kwargs))
 
-    def lprint(self, logfile, loglevel, logmsg):
-        if type(logfile) == list:
-            list(map(lambda file: self.lp(file, loglevel, logmsg), logfile))
-        else:
-            file = logfile
-            self.lp(file, loglevel, logmsg)
+    def debug(self, logfile, log_msg, **kwargs):
+        self.queue.put(
+            (logfile, sys._getframe().f_code.co_name, log_msg, kwargs))
 
-    def lp(self, file, loglevel, logmsg):
-        if file not in self.log_files_dict:
-            self.log_files_dict[file] = Logger(file)
+    def log_print(self, file, log_level, log_msg, **kwargs):
+        file = tuple(file)
+        log = Logger(file, **kwargs)
         try:
-            getattr(self.log_files_dict[file].print, loglevel)(logmsg)
+            getattr(log.print, log_level)(log_msg)
+            list(map(log.print.removeHandler, log.print.handlers))
         except Exception as err:
             print(str(err))
+
+#     def lprint(self, logfile, loglevel, logmsg):
+#         if type(logfile) == list:
+#             list(map(lambda file: self.lp(file, loglevel, logmsg), logfile))
+#         else:
+#             file = logfile
+#             self.lp(file, loglevel, logmsg)
+
+#     def lp(self, file, loglevel, logmsg):
+#         if file not in self.log_files_dict:
+#             self.log_files_dict[file] = Logger(file)
+#         try:
+#             getattr(self.log_files_dict[file].print, loglevel)(logmsg)
+#         except Exception as err:
+#             print(str(err))
 
 
 def startQueueManager(log):
@@ -198,31 +227,58 @@ def startQueueManager(log):
 def QueueManager(log):
     while True:
         try:
-            if not log.queue.empty():
-                logfile, loglevel, logmsg = log.queue.get_nowait()
-                log.lprint(logfile, loglevel, logmsg)
+            log_file, log_level, log_msg, kwargs = log.queue.get(
+                block=True, timeout=2)
+            log.queue.task_done()
+            log.log_print(log_file, log_level, log_msg, **kwargs)
+#             if not log.queue.empty():
+#                 logfile, loglevel, logmsg = log.queue.get_nowait()
+#                 log.lprint(logfile, loglevel, logmsg)
         except Exception as err:
             # print( str(err) )
             pass
 
-
+    
 class ModuleLogger:
-    def __init__(self, logger_manager, logfiles=[]):
+    def __init__(self, logger_manager, logfiles=[], **kwargs):
         self.log = logger_manager
         self.files = logfiles
         self.print = self
+        self.append2formatter = kwargs if kwargs else {}
+
+    def add_fields2formater(self, kwargs):
+        self.append2formatter = kwargs
+
+    def update_files(self, logfiles):
+        self.files = logfiles
 
     def getLoggerMannager(self):
         return self.log
 
-    def info(self, logmsg):
-        list(map(lambda file: self.log.info(file, logmsg), self.files))
+    def update_formatter(self, call_func):
+        self.append2formatter['func_Name'] = call_func.f_code.co_name
+        self.append2formatter['line_no'] = call_func.f_lineno
+        self.append2formatter['call_module'] = call_func.f_globals['__name__']
+        self.append2formatter['call_module'] = self.append2formatter[
+            'call_module'].split('.')[-1]
+        self.append2formatter['path_name'] = call_func.f_globals['__file__']
 
-    def error(self, logmsg):
-        list(map(lambda file: self.log.error(file, logmsg), self.files))
+    def info(self, log_msg):
+        self.update_formatter(sys._getframe().f_back)
+        self.log.info(self.files, log_msg, **self.append2formatter)
 
-    def warning(self, logmsg):
-        list(map(lambda file: self.log.warning(file, logmsg), self.files))
+    def error(self, log_msg):
+        self.update_formatter(sys._getframe().f_back)
+        self.log.error(self.files, log_msg, **self.append2formatter)
 
-    def success(self, logmsg):
-        list(map(lambda file: self.log.success(file, logmsg), self.files))
+    def warning(self, log_msg):
+        self.update_formatter(sys._getframe().f_back)
+        self.log.warning(self.files, log_msg, **self.append2formatter)
+
+    def success(self, log_msg):
+        self.update_formatter(sys._getframe().f_back)
+        self.log.success(self.files, log_msg, **self.append2formatter)
+
+    def debug(self, log_msg):
+        self.update_formatter(sys._getframe().f_back)
+        self.log.debug(self.files, log_msg, **self.append2formatter)
